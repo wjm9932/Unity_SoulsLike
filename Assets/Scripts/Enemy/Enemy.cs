95f8b1b5584ca897d742643fdf591bdaefa8eed5
Jaemin <wjm9932@gmail.com>
2024-04-07 23:30:26 -0700

    Change enemy path finding logic to add Physics

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -8,54 +8,97 @@ public class Enemy : LivingEntity
     public NavMeshAgent navMesh { get; private set; }
     public Rigidbody rb { get; private set; }
     public Animator animator { get; private set; }
+    public NavMeshPath path { get;  private set; }
 
     [SerializeField]
     private Character character;
     private EnemyBehaviorStateMachine enemyBehaviorStateMachine;
+
+    private Vector3 currDest;
+    private int index = 1;
     private void Awake()
     {
         rb = GetComponent<Rigidbody>();
         animator = GetComponent<Animator>();
         navMesh = GetComponent<NavMeshAgent>();
+        path = new NavMeshPath();
     }
     // Start is called before the first frame update
     void Start()
     {
-        //navMesh.updatePosition = false;
-        enemyBehaviorStateMachine = new EnemyBehaviorStateMachine(this, character);
-        enemyBehaviorStateMachine.ChangeState(enemyBehaviorStateMachine.idleState);
+        navMesh.updatePosition = false;
+        //enemyBehaviorStateMachine = new EnemyBehaviorStateMachine(this, character);
+        //enemyBehaviorStateMachine.ChangeState(enemyBehaviorStateMachine.idleState);
     }
 
     // Update is called once per frame
     void Update()
     {
-        //if (Input.GetKeyDown(KeyCode.V))
-        //{
-        //    rb.AddForce(transform.forward * 10f, ForceMode.Impulse);
-        //}
-        //navMesh.nextPosition = transform.position;
-
-        //transform.position = Vector3.MoveTowards(transform.position, character.transform.position, Time.deltaTime * 1f);
-        enemyBehaviorStateMachine.Update();
+        transform.rotation = Quaternion.Slerp(transform.rotation, GetAngle(), Time.deltaTime * 5);
+        GetPath();
+        currDest = path.corners[index];
+        if (IsArrivedCurrentDestination())
+        {
+            ++index;
+        }
+        else
+        {
+            Vector3 direction = (currDest - transform.position).normalized;
+            transform.position += direction * 3f * Time.deltaTime;
+        }
+        //enemyBehaviorStateMachine.Update();
     }
     private void FixedUpdate()
     {
-        enemyBehaviorStateMachine.PhysicsUpdate();
+        //enemyBehaviorStateMachine.PhysicsUpdate();
     }
     private void LateUpdate()
     {
-        enemyBehaviorStateMachine.LateUpdate();
+        //enemyBehaviorStateMachine.LateUpdate();
     }
     private void OnAnimationEnterEvent()
     {
-        enemyBehaviorStateMachine.OnAnimationEnterEvent();
+        //enemyBehaviorStateMachine.OnAnimationEnterEvent();
     }
     private void OnAnimationExitEvent()
     {
-        enemyBehaviorStateMachine.OnAnimationExitEvent();
+        //enemyBehaviorStateMachine.OnAnimationExitEvent();
     }
     private void OnAnimationTransitionEvent()
     {
-        enemyBehaviorStateMachine.OnAnimationTransitionEvent();
+        //enemyBehaviorStateMachine.OnAnimationTransitionEvent();
+    }
+
+    void GetPath()
+    {
+        navMesh.nextPosition = transform.position;
+        Clear();
+        navMesh.CalculatePath(character.transform.position, path);
+    }
+
+    bool IsArrivedCurrentDestination()
+    {
+        if(Vector3.Distance(currDest, transform.position) < 0f)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    private void Clear()
+    {
+        path.ClearCorners();
+        index = 1;
+    }
+
+    private Quaternion GetAngle()
+    {
+        Vector3 direction = (currDest - transform.position).normalized;
+        direction.y = 0;
+
+        return Quaternion.LookRotation(direction);
     }
 }
