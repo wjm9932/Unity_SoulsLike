6c8d9681c70cf6a2272420521e4a551618ab698a
Jaemin <wjm9932@gmail.com>
2024-08-30 23:40:59 +0900

    Remove boss RigidBody

--- Assets/Scripts/FSM/PlayerFSM/MovementState/PlayerMovementState.cs
+++ Assets/Scripts/FSM/PlayerFSM/MovementState/PlayerMovementState.cs
@@ -70,10 +70,6 @@ public abstract class PlayerMovementState : IState
         if (sm.character.IsOnSlope() == true)
         {
             sm.character.rb.AddForce(GetSlopeMoveDirection() * moveSpeed * 10f, ForceMode.Force);
-            if (sm.character.rb.velocity.y > 5)
-            {
-                sm.character.rb.AddForce(Vector3.down * 80f, ForceMode.Force);
-            }
         }
         else
         {
