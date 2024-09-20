338d9098c9a2e286ff1f534c69f9224060e29056
Jaemin <wjm9932@gmail.com>
2024-09-20 14:36:42 +0900

    Make only usable item can be dropped on quick slot

--- Assets/Scripts/Item/UI/UI_InventorySlot.cs
+++ Assets/Scripts/Item/UI/UI_InventorySlot.cs
@@ -4,12 +4,32 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 public class UI_InventorySlot : MonoBehaviour, IDropHandler
 {
+    public enum SlotType
+    {
+        QuickSlot,
+        InventorySlot
+    }
+    
+    [SerializeField]
+    SlotType slotType;
+
     public void OnDrop(PointerEventData eventData)
     {
         if (transform.childCount == 0)
         {
-            DraggableItem dropped = eventData.pointerDrag.gameObject.GetComponent<DraggableItem>();
-            dropped.originParent = transform;
+            if(slotType == SlotType.QuickSlot)
+            {
+                if(eventData.pointerDrag.gameObject.GetComponent<UsableItem>() != null)
+                {
+                    DraggableItem dropped = eventData.pointerDrag.gameObject.GetComponent<DraggableItem>();
+                    dropped.originParent = transform;
+                }
+            }
+            else
+            {
+                DraggableItem dropped = eventData.pointerDrag.gameObject.GetComponent<DraggableItem>();
+                dropped.originParent = transform;
+            }
         }
     }
 }
