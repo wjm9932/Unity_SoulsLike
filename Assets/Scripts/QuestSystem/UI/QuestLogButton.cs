f0546d18eb607c1590bda463936f58202c3857fe
Jaemin <wjm9932@gmail.com>
2024-10-04 17:05:01 +0900

    Start to add quest window

---
+++ Assets/Scripts/QuestSystem/UI/QuestLogButton.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using System;
+
+public class QuestLogButton : MonoBehaviour
+{
+    public Button button { get; private set; }
+    private TextMeshProUGUI buttonText;
+    private event Action onSelectAction;
+
+    // because we're instantiating the button and it may be disabled when we
+    // instantiate it, we need to manually initialize anything here.
+    public void Initialize(string displayName, Action selectAction)
+    {
+        this.button = this.GetComponent<Button>();
+        this.buttonText = this.GetComponentInChildren<TextMeshProUGUI>();
+
+        this.buttonText.text = displayName;
+        this.onSelectAction += selectAction;
+    }
+
+    public void OnClicked()
+    {
+        Debug.Log(this.gameObject.name);
+        if(onSelectAction != null)
+        {
+            onSelectAction();
+        }
+    }
+
+    public void SetState(QuestState state)
+    {
+        switch (state)
+        {
+            case QuestState.REQUIREMENTS_NOT_MET:
+            case QuestState.CAN_START:
+                buttonText.color = Color.red;
+                break;
+            case QuestState.IN_PROGRESS:
+            case QuestState.CAN_FINISH:
+                buttonText.color = Color.yellow;
+                break;
+            case QuestState.FINISHED:
+                buttonText.color = Color.green;
+                break;
+            default:
+                Debug.LogWarning("Quest State not recognized by switch statement: " + state);
+                break;
+        }
+    }
+}
