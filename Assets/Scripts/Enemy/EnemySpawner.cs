47f2a77db42f35eeb8970386e5c16fe7ff363d20
Jaemin <wjm9932@gmail.com>
2024-10-10 17:43:55 +0900

    Implement enemySpawner

---
+++ Assets/Scripts/Enemy/EnemySpawner.cs
@@ -0,0 +1,77 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemySpawner : MonoBehaviour
+{
+    public Transform parent;
+
+    [SerializeField]
+    private GameObject enemyPrefab;
+
+
+    [SerializeField]
+    private string targetNavMeshArea;
+
+    [SerializeField]
+    private float spawnRadius = 5f;
+    private int areaMask;
+
+    [SerializeField]
+    private int targetSpawnCount = 3;
+    private List<Enemy> enemies = new List<Enemy>();
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        areaMask = NavMesh.GetAreaFromName(targetNavMeshArea);
+    }
+    void Start()
+    {
+        for(int i = 0; i < targetSpawnCount; i++)
+        {
+            SpawnEnemy();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void SpawnEnemy()
+    {
+        Vector3 spawnPosition = GetSpawnPosition();
+
+        Enemy enemy = Instantiate(enemyPrefab, spawnPosition, Quaternion.identity, parent).GetComponent<Enemy>();
+        enemies.Add(enemy);
+
+        enemy.onDeath += () => { enemies.Remove(enemy); };
+        enemy.onDeath += () => { StartCoroutine(SpawnEnemyAfterDelay(5f)); };
+    }
+
+    private Vector3 GetSpawnPosition()
+    {
+        NavMeshHit hit;
+        Vector3 randomDirection = Random.insideUnitSphere * spawnRadius + transform.position;
+
+        if (NavMesh.SamplePosition(randomDirection, out hit, spawnRadius, 1 << areaMask) == true)
+        {
+            return hit.position; 
+        }
+        else
+        {
+            Debug.LogError("cannot find valid position");
+            return transform.position;
+        }
+
+    }
+
+    private IEnumerator SpawnEnemyAfterDelay(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        SpawnEnemy(); 
+    }
+}
