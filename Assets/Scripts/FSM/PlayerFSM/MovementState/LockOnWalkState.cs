d4812e7a7b034e0293deb0cb3ade759e65ee9015
Jaemin <wjm9932@gmail.com>
2024-11-06 10:31:38 +0900

    .

--- Assets/Scripts/FSM/PlayerFSM/MovementState/LockOnWalkState.cs
+++ Assets/Scripts/FSM/PlayerFSM/MovementState/LockOnWalkState.cs
@@ -20,6 +20,8 @@ namespace PlayerFSM
 
         public override void Update()
         {
+            sm.owner.cameraTransform.localPosition = Vector3.Lerp(sm.owner.cameraTransform.localPosition, sm.owner.originCameraTrasform, 2f * Time.deltaTime);
+
             if (sm.owner.input.isSprinting == true)
             {
                 sm.ChangeState(sm.sprintState);
