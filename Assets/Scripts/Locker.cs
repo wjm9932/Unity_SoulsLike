703b30bc0daa268c62a3933bdc1ac22d5712df9d
Jaemin <wjm9932@gmail.com>
2024-11-18 10:38:48 +0900

    add ease out when door open

--- Assets/Scripts/Locker.cs
+++ Assets/Scripts/Locker.cs
@@ -1,6 +1,5 @@
 using System;
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(SphereCollider))]
@@ -10,27 +9,15 @@ public class Locker : MonoBehaviour
     [SerializeField] private GameObject key;
 
     private Action unlockAction;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
     private void Unlock(Character player)
     {
-        if(player.inventory.GetTargetItemCountFromInventory(key.GetComponent<UX.UX_Item>()) >= 1)
+        if (player.inventory.GetTargetItemCountFromInventory(key.GetComponent<UX.UX_Item>()) >= 1)
         {
             player.playerEvents.onUnlock -= unlockAction;
             player.GetComponent<InteractionIndicator>().Hide();
-            this.transform.rotation *= Quaternion.Euler(0, 90, 0);
-            minimapIcon.SetActive(false);
-            Destroy(this);
+
+            StartCoroutine(RotateOverTime(Quaternion.Euler(0, 90, 0), 2f));
         }
         else
         {
@@ -39,15 +26,37 @@ public class Locker : MonoBehaviour
         }
     }
 
+    private IEnumerator RotateOverTime(Quaternion targetRotation, float duration)
+    {
+        Quaternion initialRotation = transform.rotation;
+        float elapsedTime = 0f;
+
+        while (elapsedTime < duration)
+        {
+            elapsedTime += Time.deltaTime;
+
+            float percentage = elapsedTime / duration;
+            percentage = Mathf.Pow(percentage, 1f / 2.5f);
+
+            transform.rotation = Quaternion.Slerp(initialRotation, initialRotation * targetRotation, percentage);
+            yield return null;
+        }
+
+        transform.rotation = initialRotation * targetRotation;
+        minimapIcon.SetActive(false);
+        Destroy(this);
+    }
+
     private void OnTriggerEnter(Collider other)
     {
-        if(other.GetComponent<Character>() != null)
+        if (other.GetComponent<Character>() != null)
         {
             unlockAction = () => { Unlock(other.GetComponent<Character>()); };
             other.GetComponent<Character>().playerEvents.onUnlock += unlockAction;
             other.GetComponent<Character>().GetComponent<InteractionIndicator>().Show();
         }
     }
+
     private void OnTriggerExit(Collider other)
     {
         if (other.GetComponent<Character>() != null)
